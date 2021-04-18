d24fbf0de9333163567a04a95660e3ead7a707ee
smrswnt20 <65486271+smrswnt20@users.noreply.github.com>
2021-04-18 13:12:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const http = require('http');
 const fs = require("fs");
-var request = require("")
+var request = require("requests");
+
+const replaceVal = (tempVal,orgVal) => {
+    let temperature= tempVal.replace("{%tempval%}",orgVal.main.temp);
+}
 
 const homeFile = fs.readFileSync("home.html", "utf-8");
 
@@ -8,7 +12,11 @@ const server =http.createServer((req,res) => {
     if (req.url == "/") {
         requests("http://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=eed390a5c5768b10802ef8fca4de8e3b"); //api from openweather.com with my personal key
         .on("data", (chunk) =>{
-            console.log(chunk);
+            const objdata = JSON.parse(chunk);
+            const arrData = [objdata];
+            const realTimeData = arrData.map((val) => {
+                replaceVal(homeFile, val);
+            });
         })
         .on("end", (err) => {
         if (err) return console.log("connection closed due to errors",err);
@@ -18,3 +26,4 @@ const server =http.createServer((req,res) => {
 
     }
 }
+server.listen(8000,"127.0.0.1");
